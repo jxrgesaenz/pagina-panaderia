f4212421ce9481a2658d2956b58d92b703444518
Jorge saenz <jorgesaenz@gmail.com>
2020-12-12 16:52:22 -0700

    Version final

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use(require("./rutas/paginas"))
-
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto: "+PORT)
